6020104b92dbec04e3ba7b55784ed271831b8b15
Damiano Cacchiarelli <damiano.cacchiarelli@studenti.unicam.it>
2023-04-13 19:27:01 +0200

    Added rate for worker_eat and queen_eat

--- honey-hive.pm
+++ honey-hive.pm
@@ -9,17 +9,17 @@ const STATES = 3;                           /* Worker has 4 STATES: Rest (0), Fi
 const MAX_STORAGE = 10000;
 const MAX_BEE_POPULATION = 10000;
 const SPECIES = 2;
-const WATER_CONCENTRATION = 90;
+const WATER_CONCENTRATION = 9;
 const NECTAR_AVAILABLE = 2;
 const DISEASE = 10;
 const NECTAR_BEE_STORAGE = 10;              /* A bee need 10 flowes to fill its nectar storage*/
 
 /* --------------------------------- Agents --------------------------------- */
 species Q of [0, ENERGY];                                                         /* Queen */
-species WR of [0, ENERGY]*[0, NECTAR_BEE_STORAGE]*[0, DISEASE];                   /* Worker in Rest state */
+species WR of [0, ENERGY]*[0, DISEASE];                                           /* Worker in Rest state */
 species WF of [0, ENERGY]*[0, NECTAR_BEE_STORAGE]*[0, DISEASE];                   /* Worker in Find state */
 species WS of [0, ENERGY]*[0, NECTAR_BEE_STORAGE]*[0, DISEASE];                   /* Worker in Store state */
-species WP of [0, ENERGY]*[0, NECTAR_BEE_STORAGE]*[0, DISEASE];                   /* Worker in Produce state */
+species WP of [0, ENERGY]*[0, DISEASE];                                           /* Worker in Produce state */
 species H;                                                                        /* Honey Storage */
 species N;                                                                        /* Nectar Storage */
 species F of [0, SPECIES]*[0, WATER_CONCENTRATION]*[0, NECTAR_AVAILABLE];         /* Flowers */
@@ -30,15 +30,15 @@ species DQ; /* Death Worker */
 
 /* ---------------------------------- Rate ---------------------------------- */
 
-const bee_birth_rate = 0.5 ;
+const bee_birth_rate = 0.9 ;
 const bee_mortality_rate = 0.2 ;
 const queen_mortality_rate = 0.01 ;
 
 const queen_metabolism = 0.15 ;
 const bee_metabolism = 0.25 ;
-const eat_rate = 1;
+const eat_rate = 0.9 ;
 
-const bee_change_state_rate = 0.9;
+const bee_change_state_rate = 0.9 ;
 
 const flower_pruduce_nectar_rate = 0.25 ;
 
@@ -51,10 +51,10 @@ const pesticide_exposure = 0.01 ;
 /* -------------------------------------------------------------------------- */
 
 label flowers_nectar_available = { F[i, j, 1 for i in [0,SPECIES] and j in [0,WATER_CONCENTRATION]] }
-label workers_in_rest = { WR[i, j, z for i in [1,ENERGY] and j in [0,NECTAR_BEE_STORAGE] and z in [0,DISEASE]] }
+label workers_in_rest = { WR[i, z for i in [1,ENERGY] and j in [0,NECTAR_BEE_STORAGE] and z in [0,DISEASE]] }
 label workers_in_find = { WF[i, j, z for i in [1,ENERGY] and j in [0,NECTAR_BEE_STORAGE] and z in [0,DISEASE]] }
 label workers_in_store = { WS[i, j, z for i in [1,ENERGY] and j in [0,NECTAR_BEE_STORAGE] and z in [0,DISEASE]] }
-label workers_in_produce = { WP[i, j, z for i in [1,ENERGY] and j in [0,NECTAR_BEE_STORAGE] and z in [0,DISEASE]] }
+label workers_in_produce = { WP[i, z for i in [1,ENERGY] and j in [0,NECTAR_BEE_STORAGE] and z in [0,DISEASE]] }
 
 
 /* -------------------------------------------------------------------------- */
@@ -69,13 +69,13 @@ Queen Eat food:
     Descrease Honey Storage (H);
 
 Base rates: 
-    queen_metabolism;
+    eat_rate;
 
 Impacts:
     Residual ENERGY;
 */
-rule queen_eat_food for i in [1, ENERGY-1]{
-    Q[i] | H<4*0.5> -[ 0.5 ]-> Q[i+1]
+rule queen_eat_food for e in [1, ENERGY-1]{
+    Q[e] | H<1> -[ eat_rate * (1-1.5^(e-ENERGY))) ]-> Q[e+1]
 }
 
 /* 
@@ -90,8 +90,8 @@ Impacts:
     Residual ENERGY (Low ENERGY decrease bee_birth_rate);
     (*) Number of Worker (High number of Worker decrease bee_birth_rate);
 */
-rule queen_generate_worker for i in [1, ENERGY] {
-    Q[i] -[ 0.5 ]-> Q[i-1] | WR[5, 0, 0]
+rule queen_generate_worker for e in [1, ENERGY] {
+    Q[e] -[ bee_birth_rate * 1.7^(e-ENERGY) ]-> Q[e-1] | WR[5, 0]
 }
 
 /*
@@ -107,17 +107,17 @@ rule queen_dies {
 Worker dies:
     The residual ENERGY is 0;
 */
-rule worker_rest_dies for j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE]{
-    WR[0,j,z] -[ 1 ]-> DW
+rule worker_rest_dies for d in [0, DISEASE] {
+    WR[0,d] -[ 1 ]-> DW
 }
-rule worker_find_dies for j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE]{
-    WF[0,j,z] -[ 1 ]-> DW
+rule worker_find_dies for n in [0, NECTAR_BEE_STORAGE] and d in [0, DISEASE]{
+    WF[0,n,d] -[ 1 ]-> DW
 }
-rule worker_store_dies for j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE]{
-    WS[0,j,z] -[ 1 ]-> DW
+rule worker_store_dies for n in [0, NECTAR_BEE_STORAGE] and d in [0, DISEASE]{
+    WS[0,n,d] -[ 1 ]-> DW
 }
-rule worker_produce_dies for j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE]{
-    WP[0,j,z] -[ 1 ]-> DW
+rule worker_produce_dies for d in [0, DISEASE] {
+    WP[0,d] -[ 1 ]-> DW
 }
 
 
@@ -135,8 +135,8 @@ Impacts:
     Residual ENERGY (Low ENERGY decrease bee_change_state_rate);
     (*) Residual HONEY (H) in the hive;
 */
-rule worker_rest_to_find for i in [1, ENERGY] and j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE] {
-    WR[i,j,z] -[bee_change_state_rate * (i/ENERGY)]-> WF[i-1,j,z]
+rule worker_rest_to_find for e in [1, ENERGY] and d in [0, DISEASE] {
+    WR[e,d] -[bee_change_state_rate * (e/ENERGY)]-> WF[e-1,0,d]
 }
 
 /*
@@ -150,8 +150,8 @@ Impacts:
     Residual ENERGY (Low ENERGY decrease bee_meets_flower_rate);
     Residual Flowers with NECTAR_AVAILABLE;
 */
-rule worker_meets_flower for i in [1, ENERGY-1] and j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE] and a in [0, SPECIES] and b in [0, WATER_CONCENTRATION]{
-    WF[i,j,z] | F[a,b,1] -[%flowers_nectar_available * (i/ENERGY)]-> WF[i,j+1,z] | F[a,b,0]
+rule worker_meets_flower for e in [1, ENERGY-1] and n in [0, NECTAR_BEE_STORAGE-1] and d in [0, DISEASE] and s in [0, SPECIES] and w in [0, WATER_CONCENTRATION]{
+    WF[e,n,d] | F[s,w,1] -[(flowers_nectar_available/#F) * (e/ENERGY)]-> WF[e,n+1,d] | F[s,w,0]
 }
 
 /*
@@ -164,8 +164,8 @@ Base rates:
 Impacts:
 
 */
-rule worker_exposed_pesticide for i in [1, ENERGY] and j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE-1] {
-    WF[i,j,z] -[pesticide_exposure]-> WF[i,j,z+1]
+rule worker_exposed_pesticide for e in [1, ENERGY] and n in [0, NECTAR_BEE_STORAGE] and d in [0, DISEASE-1] {
+    WF[e,n,d] -[pesticide_exposure]-> WF[e,n,d+1]
 }
 
 /* ----------------------------- Worker in STORE ---------------------------- */
@@ -181,8 +181,8 @@ Impacts:
     Residual ENERGY (Low ENERGY increase bee_change_state_rate);
     Level of NECTAR_BEE_STORAGE (Low NECTAR_BEE_STORAGE decrease bee_change_state_rate);
 */
-rule worker_find_to_store for i in [1, ENERGY] and j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE] {
-    WF[i,j,z] -[bee_change_state_rate * ((ENERGY-i)/ENERGY) * (j/NECTAR_BEE_STORAGE)]-> WS[i,j,z]
+rule worker_find_to_store for e in [1, ENERGY] and n in [0, NECTAR_BEE_STORAGE] and d in [0, DISEASE] {
+    WF[e,n,d] -[bee_change_state_rate * ((ENERGY-e)/ENERGY) * (n/NECTAR_BEE_STORAGE)]-> WS[e,n,d]
 }
 
 /* 
@@ -191,8 +191,8 @@ Worker (WS) store Nectar (N):
     Increase Nectar storage (N);  
 
 TODO */
-rule worker_store_nectar for i in [1, ENERGY] and j in [1, NECTAR_BEE_STORAGE] and z in [0, DISEASE] {
-    WS[i,j,z] -[1]-> WS[i-1,0,z] | N<j>
+rule worker_store_nectar for e in [1, ENERGY] and n in [1, NECTAR_BEE_STORAGE] and d in [0, DISEASE] {
+    WS[e,n,d] -[1]-> WS[e-1,0,d] | N<n>
 }
 
 /* ---------------------------- Worker in PRODUCE --------------------------- */
@@ -200,16 +200,16 @@ rule worker_store_nectar for i in [1, ENERGY] and j in [1, NECTAR_BEE_STORAGE] a
 /*
 Worker from STORE (WS) go to PRODUCE (WP) state:
     Bee change state;
-    Decrease bee ENERGY;
 
 Base rates:
     bee_change_state_rate;
 
 Impacts:
 
+TODO
 */
-rule worker_store_to_produce for i in [1, ENERGY] and z in [0, DISEASE] {
-    WS[i,0,z] -[bee_change_state_rate]-> WP[i-1,0,z]
+rule worker_store_to_produce for e in [1, ENERGY] and n in [0, NECTAR_BEE_STORAGE] and d in [0, DISEASE] {
+    WS[e,n,d] -[bee_change_state_rate]-> WP[e,d]
 }
 
 /* 
@@ -222,8 +222,8 @@ TODO :
     Use H[] in order to store decimal numbers??
     Humidity impacts honey production??
 */
-rule worker_produce_honey for i in [1, ENERGY] and j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE] {
-    WP[i,j,z] | N<4> -[1]-> WP[i-1,j,z] | H<1>
+rule worker_produce_honey for e in [1, ENERGY] and d in [0, DISEASE] {
+    WP[e,d] | N<4> -[1]-> WP[e-1,d] | H<1>
 }
 
 
@@ -240,8 +240,8 @@ Impacts:
     Level of NECTAR (N) (Low NECTAR increase bee_change_state_rate);
     Residual ENERGY (Low ENERGY increase bee_change_state_rate);
 */
-rule worker_produce_to_rest for i in [1, ENERGY] and j in [0, NECTAR_BEE_STORAGE] and z in [0, DISEASE] {
-    WP[i,j,z] -[bee_change_state_rate]-> WR[i,j,z]
+rule worker_produce_to_rest for e in [1, ENERGY] and d in [0, DISEASE] {
+    WP[e,d] -[bee_change_state_rate]-> WR[e,d]
 }
 
 /*
@@ -254,8 +254,8 @@ BaseRate:
 Impacts:
     Residual ENERGY (Low ENERGY decrease eat_rate);
 */
-rule worker_eat for i in [1, ENERGY-1] and j in [1, NECTAR_BEE_STORAGE] and z in [0, DISEASE] {
-    WR[i,j,z]<5> | H -[(1-(i/ENERGY))*eat_rate]-> WR[i+1,j,z]<5>
+rule worker_eat for e in [1, ENERGY-1] and d in [0, DISEASE] {
+    WR[e,d] | H -[eat_rate*(1-1.5^(e-ENERGY))]-> WR[e+1,d]
 } 
 
 
@@ -271,8 +271,8 @@ Base rates:
 Impacts:
     Wather condition;
 */
-rule flower_produce_nectar for i in [0, SPECIES] and j in [0, WATER_CONCENTRATION]{
-    F[i,j,0] -[flower_pruduce_nectar_rate]-> F[i,j,1]
+rule flower_produce_nectar for s in [0, SPECIES] and w in [0, WATER_CONCENTRATION]{
+    F[s,w,0] -[flower_pruduce_nectar_rate]-> F[s,w,1]
 }
 
 
@@ -280,12 +280,16 @@ rule flower_produce_nectar for i in [0, SPECIES] and j in [0, WATER_CONCENTRATIO
 /*                            MEASURES & PREDICATE                            */
 /* -------------------------------------------------------------------------- */
 
-measure n_worker = (#workers_in_find + #workers_in_find + #workers_in_store + #workers_in_produce);
+measure n_worker = (#workers_in_rest + #workers_in_find + #workers_in_store + #workers_in_produce);
+measure workers_in_rest = #workers_in_rest;
+measure workers_in_find = #workers_in_find;
+measure workers_in_store = #workers_in_store;
+measure workers_in_produce = #workers_in_produce;
 measure honey_available = #H;
 measure nectar_available = #N;
 measure flower_with_nectar = #flowers_nectar_available;
-measure flower_without_nectar = #F[i, j, 0 for i in [0,SPECIES] and j in [0,WATER_CONCENTRATION]];
 measure worker_death = #DW;
+measure queen_death = #DQ;
 
 /* 
 predicate honey_decrise = (#HS < 50);
@@ -296,4 +300,4 @@ predicate workers = ( #W[i for i in [0,STATES], j for j in [0,ENERGY], z for z i
 /* -------------------------------------------------------------------------- */
 /*                                   SYSTEM                                   */
 /* -------------------------------------------------------------------------- */
-system initial = Q[ENERGY/2]<1> | H<50> | N<1000> | F[0,70,1]<500> | F[1,80,1]<500>;
+system initial = Q[ENERGY/2]<1> | WR[ENERGY-1, 0]<10000> | H<500> | N<50> | F[0,7,1]<500> | F[1,8,1]<500>;
