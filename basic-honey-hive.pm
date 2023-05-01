7b59c3a825f3264247a6ffdd9aec3eb35399b3e5
Damiano Cacchiarelli <damiano.cacchiarelli@studenti.unicam.it>
2023-05-02 00:41:56 +0200

    Added comments

--- basic-honey-hive.pm
+++ basic-honey-hive.pm
@@ -11,7 +11,7 @@ const SPECIES = 3;                          /* The max number of flower species 
 const NECTAR_AVAILABLE = 2;                 /* 1 if the flower has nectar available; 0 if not available */
 
 /*Hive*/            
-const FOOD_STORAGE = 1000;                  /* The amount of nectar that can be stored */
+const FOOD_STORAGE = 1000;                  /* The amount of nectar that can be stored in the hive */
 
 /* --------------------------------- Agents --------------------------------- */
 
@@ -23,21 +23,21 @@ species N;                                              /* Nectar Storage */
 
 species F of [0, SPECIES]*[0, NECTAR_AVAILABLE];        /* Flowers */
 
-species DQ;                                             /* Death Queen */
-species DW;                                             /* Death Worker */
+/* Secondary Agents */
+species N_F;   /* Nectar used for food */
+species N_H;   /* Nectar used for Honey */
+species N_QF;  /* Nectar used for Queen food */
 
-/*Debug*/
-species N_F;   /*Nectar used for food */
-species N_H;   /*Nectar used for Honey*/
-species N_QF;  /*Nectar used for Queen food */
-
-species BB;    /*Born Bees*/
+species DQ;    /* Death Queen */
+species DW;    /* Death Worker */
+species BB;    /* Born Bees */
 
 /* ---------------------------------- Rates ---------------------------------- */
 
 const eat_rate = 0.90 ;
 
-const queen_mortality_rate = 0.0001 ;
+const queen_mortality_rate = 0.0001 ;       /* The probability that the queen dies is pretty low. 
+                                               The queen will die only when the colony is dying and it remains alone. */
 const queen_metabolism = 0.25 ;
 const bee_birth_rate = 1.0 ;
 
@@ -52,16 +52,19 @@ const flower_pruduce_nectar_rate = 0.2 ;
 
 /* --------------------------------- Params --------------------------------- */
 
-param temperature = 31 ;
-param rainfall_rate = 1;
-param humidity = 5 ;
-param flowers_biodiversity = 1 ;
-param pesticide_exposure_rate = 0.0 ;
+param temperature = 40 ;                    /* Ranges between 5 and 60. The ideal_temperature is 31 degrees */
+param rainfall_rate = 1;                    /* The rateo of rainfall; values between 0 and 1 */
+param humidity = 1 ;                        /* Represent the percentage of humidity; values between 0 and 9; The ideal_humidity is 5; */
+param flowers_biodiversity = 1 ;            /* The number of species of flowers */
+param pesticide_exposure_rate = 0.5 ;       /* The rate at with the workers are exposed to pesticide; Ranges between 0 and 1: 0 means no exposure; */
 
 /* ---------------------------------- Math functions --------------------------------- */
 
 /*
 {ID: WF_1} Workers population function:
+
+Used to increase the probability of the death of the queen; When the population falls below
+one third of critical_workers_population, the probability od dying increases exponentially.
 
     1/4^(#workers - critical_workers_population/3)
 
@@ -78,7 +81,7 @@ const critical_workers_population = 10;
 /*
 {ID: HF_1} Humidity function:
 
-Describe the probability to produce honey at a given humidity;
+Describe the influences of humidity on the production of honey; Used to decrease the honey production.
 
     1/(humidity - ideal_humidity)^2humidity_impact
 
@@ -96,20 +99,24 @@ humidity_impact:
 */
 const ideal_humidity = 5;
 const humidity_impact = 1;
-const humidity_rate = 1/(((humidity - ideal_humidity)^2*humidity_impact)+1);
+const humidity_influence = 1/(((humidity - ideal_humidity)^(2*humidity_impact))+1);
 
 
 /*
 {ID: TF_1} Temperature function:
 
-Describe the probability of death of a bee at a given temperature;
+Describe the influences of temperature on the lifetime of a worker (WB); Used to increase the probability
+of death of workers.
 
     0.9 - 0.9/(1 + (2*(temperature - ideal_temperature)/delta_temperature)^2temperature_impact)
 
 temperature:
     The temperature in the hive;
     Decimal; Renges between 5 and 60 -> [5, 60];
- 
+
+delta_temperature:
+    The maximum variation of degrees from ideal_temperature in with the probability of death is low; 
+
 ideal_temperature:
     Represent the ideal temperature in the hive for the bees survival;
 
@@ -122,7 +129,7 @@ temperature_impact:
 const ideal_temperature = 31;
 const delta_temperature = 8;
 const temperature_impact = 2;
-const temperature_rate = 1 - 0.9/(1 + (2*(temperature - ideal_temperature)/delta_temperature)^(2*temperature_impact));
+const temperature_influence = 1 - 0.9/(1 + ((temperature - ideal_temperature)/delta_temperature)^(2*temperature_impact));
 
 /* -------------------------------------------------------------------------- */
 /*                                   LABELS                                   */
@@ -149,7 +156,7 @@ label used_storage = { N }
 >> QUEEN EATS <<
 
 Results:
-    Increase ENERGY;
+    Increase Queen (Q) ENERGY;
     Descrease NECTAR Storage (N);
 
 Base rates: 
@@ -157,8 +164,6 @@ Base rates:
 
 Impacts:
     Residual ENERGY (Low ENERGY increase eat_rate);
-
-    (1-1.5^(e-ENERGY))
 */
 rule queen_eats for e in [0, ENERGY-2]{
     Q[e] | N<3> -[ eat_rate * (e/(ENERGY-1))]-> Q[e+2] | N_QF<3>
@@ -195,10 +200,10 @@ rule queen_dies for e in [0, ENERGY]{
 }
 
 /*
->> QUEEN   generate WORKER <<
+>> QUEEN generate WORKER <<
 
 Results:
-    Drecrease Queen ENERGY;
+    Drecrease Queen (Q) ENERGY;
     Increase number of Worker (WB);
 
 Base rates: 
@@ -206,10 +211,10 @@ Base rates:
 
 Impacts: 
     Food availbility (Low Food decrease bee_birth_rate);
-    Number of Worker (High number of Worker decrease bee_birth_rate);
+    Number of Worker (High number of Worker compared to food decrease bee_birth_rate);
 */
 rule queen_generate_worker for e in [1, ENERGY] {
-    Q[e] -[ bee_birth_rate * (e/ENERGY) * ((#N+1)/FOOD_STORAGE)]-> Q[e] | WB[ENERGY-1, 0]<2> | BB<2>
+    Q[e] -[ bee_birth_rate * (e/ENERGY) * ((#N+1)/FOOD_STORAGE) * (1- #workers//#N)*2]-> Q[e] | WB[ENERGY-1, 0]<2> | BB<2>
 }
 
 
@@ -258,9 +263,10 @@ Impacts:
     Residual ENERGY (Low ENERGY increase worker_mortality_rate);
     POISONING level (High level of POISONING increase worker_mortality_rate);
     Temperature impact {TF_1};
+    Level of flowers_biodiversity;
 */
 rule worker_dies for e in [0, ENERGY] and p in [0, POISONING] {
-    WB[e,p] -[ worker_mortality_rate * (1 - e/ENERGY) + 4^(p - POISONING) + temperature_rate + (1- flowers_biodiversity/SPECIES)/5]-> DW
+    WB[e,p] -[ worker_mortality_rate * (1 - e/ENERGY) + (4^(p - POISONING) + temperature_influence)/2 + (1- flowers_biodiversity/SPECIES)/5]-> DW
 }
 
 /*
@@ -291,7 +297,7 @@ Base rates:
     pesticide_exposure_rate;
 */
 rule worker_exposed_pesticide for e in [0, ENERGY] and p in [0, POISONING-1] {
-    WB[e,p] -[pesticide_exposure_rate]-> WB[e,p+1]
+    WB[e,p] -[pesticide_exposure_rate/2]-> WB[e,p+1]
 }
 
 /*
@@ -307,10 +313,11 @@ Base rates:
 Impacts:
     Residual ENERGY (Low ENERGY decrease worker_produce_honey_rate);
     Humidity impact {HF_1};
-
+    Food availbility (Low Food decrease worker_produce_honey_rate);
+    Number of Worker (High number of Worker compared to the food decrease worker_produce_honey_rate);
 */
 rule worker_produce_honey for e in [1, ENERGY] and p in [0, POISONING]{
-    WB[e,p] | N<2> -[worker_produce_honey_rate * humidity_rate/2 * (e/ENERGY) * (#N/(FOOD_STORAGE))]-> WB[e,p] | H<1> | N_H<2>
+    WB[e,p] | N<2> -[worker_produce_honey_rate * humidity_influence/2 * (e/ENERGY) * (#N/(FOOD_STORAGE)) * (1- #workers//#N)*2]-> WB[e,p] | H<1> | N_H<2>
 }
 
 
@@ -344,7 +351,7 @@ measure workers_death = #DW;
 measure worker_death_x_born = #DW/#BB;
 measure queen_death = #DQ;
 
-/*Debug*/
+/* Others*/
 measure nectar_food = #N_F;
 measure nectar_honey = #N_H;
 measure nectar_queen_food = #N_QF;
@@ -356,11 +363,7 @@ measure poisoned_workers = #infected_workers;
 
 /* -------------------------------- Predicate ------------------------------- */
 
-predicate colony_survived = (#Q[e for e in [0, ENERGY]] > 0);
-/* 
-predicate honey_decrise = (#HS < 50);
-predicate workers = ( #W[i for i in [0,STATES], j for j in [0,ENERGY], z for z in [0,POISONING]]>0);
-*/
+predicate hive_survived = (#Q[e for e in [0, ENERGY]] > 0);
 
 /* -------------------------------------------------------------------------- */
 /*                                   SYSTEM                                   */
@@ -373,10 +376,10 @@ system old_hive = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/
 /* ------------------------------- Arid biomes ------------------------------ */
 
 /* 
-Desert biome: few flowers, a hot, dry climate and low rainfall;
+Desert biome: very few flowers, a hot and dry climate;
 
 Params:
-    temperature = 45; 
+    temperature = 40; 
     rainfall_rate = 1;
     humidity = 1;
     flowers_biodiversity = 1 ;
@@ -386,7 +389,7 @@ system new_hive_desert = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>   
 system old_hive_desert = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<10>;
 
 /*
-Savanna biome: 
+Savanna biome: very few flowers, a temperate and dry climate;
 
 Params:
     temperature = 31; 
@@ -395,14 +398,14 @@ Params:
     flowers_biodiversity = 1 ;
     pesticide_exposure_rate = 0.0; 0.8;
 */
-system new_hive_savanna = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<300>;
-system old_hive_savanna = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<300>;
+system new_hive_savanna = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<10>;
+system old_hive_savanna = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<10>;
 
 /*
-Taiga biome: 
+Taiga biome: few flowers, a cool and dry climate;
 
 Params:
-    temperature = 15; 
+    temperature = 22; 
     rainfall_rate = 1;
     humidity = 1;
     flowers_biodiversity = 1 ;
@@ -414,10 +417,10 @@ system old_hive_taiga = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_ST
 /* ----------------------------- Wetland biomes ----------------------------- */
 
 /*
-Jungle biome: 
+Jungle biome: many flowers, a hot and wet climate;
 
 Params:
-    temperature = 45; 
+    temperature = 40; 
     rainfall_rate = 1;
     humidity = 9;
     flowers_biodiversity = 3 ;
@@ -427,7 +430,7 @@ system new_hive_jungle = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>   
 system old_hive_jungle = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<300> | F[1,1]<300> | F[2,1]<300>;
 
 /*
-Swamp biome: 
+Swamp biome: many flowers, a hot and wet climate;
 
 Params:
     temperature = 31; 
@@ -440,25 +443,39 @@ system new_hive_swamp = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>    
 system old_hive_swamp = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<200> | F[1,1]<200> | F[2,1]<200>;
 
 /*
-Frozen River biome: 
+Mangrove Swamp biome: very few flowers, a hot and wet climate;
 
 Params:
-    temperature = 15; 
+    temperature = 31; 
     rainfall_rate = 1;
     humidity = 9;
     flowers_biodiversity = 3 ;
     pesticide_exposure_rate = 0.0; 0.8;
 */
-system new_hive_frozen_river = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<200> | F[2,1]<200> | F[2,1]<200>;
-system old_hive_frozen_river = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<200> | F[2,1]<200> | F[2,1]<200>;
+system new_hive_mangrove_swamp = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<3> | F[1,1]<3> | F[2,1]<3>;
+system old_hive_mangrove_swamp = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<3> | F[1,1]<3> | F[2,1]<3>;
+
+
+/*
+Frozen River biome: 
+
+Params:
+    temperature = 22; 
+    rainfall_rate = 1;
+    humidity = 9;
+    flowers_biodiversity = 3 ;
+    pesticide_exposure_rate = 0.0; 0.8;
+*/
+system new_hive_frozen_river = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<200> | F[1,1]<200> | F[2,1]<200>;
+system old_hive_frozen_river = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<200> | F[1,1]<200> | F[2,1]<200>;
 
 /* ---------------------------- Temperate biomes ---------------------------- */
 
 /*
-Plains biome: 
+Plains biome: many flowers, hot and temperate climate;
 
 Params:
-    temperature = 45; 
+    temperature = 40; 
     rainfall_rate = 1;
     humidity = 5;
     flowers_biodiversity = 2 ;
@@ -468,39 +485,52 @@ system new_hive_plains = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>   
 system old_hive_plains = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<400> | F[1,1]<400>;
 
 /*
-Flower Forest biome: 
+Stony plains biome: very few flowers, and temperate climate;
 
 Params:
     temperature = 31; 
     rainfall_rate = 1;
     humidity = 5;
-    flowers_biodiversity = 2 ;
+    flowers_biodiversity = 3 ;
     pesticide_exposure_rate = 0.0; 0.8;
 */
-system new_hive_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<600> | F[1,1]<600>;
-system old_hive_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<600> | F[1,1]<600>;
+system new_hive_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<3> | F[1,1]<3> | F[2,1]<3>;
+system old_hive_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<3> | F[1,1]<3> | F[2,1]<3>;
 
 /*
-Stony Peaks biome: 
+Flower Forest biome: a lot of flowers, and temperate climate;
 
 Params:
-    temperature = 15; 
+    temperature = 31; 
+    rainfall_rate = 1;
+    humidity = 5;
+    flowers_biodiversity = 3 ;
+    pesticide_exposure_rate = 0.0; 0.8;
+*/
+system new_hive_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<400> | F[1,1]<400> | F[2,1]<400>;
+system old_hive_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<400> | F[1,1]<400> | F[2,1]<400>;
+
+/*
+Stony Peaks biome: few flowers, a cool and temperate climate;
+
+Params:
+    temperature = 22; 
     rainfall_rate = 1;
     humidity = 5;
     flowers_biodiversity = 2 ;
     pesticide_exposure_rate = 0.0; 0.8;
 */
-system new_hive_stony_peaks = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<250> | F[2,1]<250>;
-system old_hive_stony_peaks = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<250> | F[2,1]<250>;
+system new_hive_stony_peaks = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  | H<0> | N<40>             | F[0,1]<250> | F[1,1]<250>;
+system old_hive_stony_peaks = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<250> | F[1,1]<250>;
 
 /* ------------------------------ Mixed biomes ------------------------------ */
 
 /*
-Desert x Flower Forest biome: 
+Desert x Flower Forest biome: a lot of flowers, a dry and hot climate;
 
 Params:
-    temperature = 45; 
-    rainfall_rate = 0.3;
+    temperature = 40; 
+    rainfall_rate = 1;
     humidity = 1;
     flowers_biodiversity = 2 ;
     pesticide_exposure_rate = 0.0; 0.8;
@@ -509,7 +539,7 @@ system new_hive_desert_x_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<40>  |
 system old_hive_desert_x_flower_forest = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> | H<0> | N<FOOD_STORAGE/2> | F[0,1]<600> | F[1,1]<600>;
 
 /*
-Flower Forest x Desert biome: 
+Flower Forest x Desert biome: very few flowers, and temperate climate;
 
 Params:
     temperature = 31; 
@@ -526,26 +556,8 @@ system old_hive_flower_forest_x_desert = Q[ENERGY-1]<1> | WB[ENERGY-1, 0]<200> |
 /*                                TODO & NOTES                                */
 /* -------------------------------------------------------------------------- */
 
-/* 
-NOTES:
-
------------------------------------- - -----------------------------------
-Worker (WS) store NECTAR (N):
-    Decrease bee ENERGY;
-    Increase NECTAR storage (N);
-
-Base rates:
-    worker_store_nectar;
-
-
-rule worker_store_nectar for e in [1, ENERGY] and p in [0, POISONING] {
-    WB[e,p] -[worker_store_nectar * (1- #used_storage/food_storage)]-> WB[e,p] | N<1>
-}
------------------------------------- - -----------------------------------
-
-*/
-
 /*
-TODO: 
-    Possible simulation: Pesticidi?? Temperature intermedie (caldo-normale e normale-freddo)??
+
+
+
 */
